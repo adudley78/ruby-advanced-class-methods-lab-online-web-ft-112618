fce590c2d7014e65a9864575a322432dd59cc6df
Adam Dudley <dudleyadam@gmail.com>
2018-12-05 16:41:19 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,19 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new #instantiates a new instance of the Song class
+    song.save #saves the song instance to the @@all class variable by way of the save method
+    song #returns the new song as it now exists in the array assigned to @@all
+    #@@all << song
+  end
+
+  def self.new_by_name
+    create
+    #song.name = name
+    # song = self.new
+    # song.name = name
+    #song.name = name
+  end
+
 end
